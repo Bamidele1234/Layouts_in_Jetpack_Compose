9342960694cd2e9eccdde568ea529e5b3106ff75
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-03-15 20:05:54 +0100

    Added a created a custom layout

--- app/src/main/java/com/example/layoutsinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/layoutsinjetpackcompose/MainActivity.kt
@@ -3,20 +3,31 @@ package com.example.layoutsinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutsinjetpackcompose.ui.theme.LayoutsInJetpackComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,18 +80,53 @@ fun ImageListItem(index: Int){
 }
 
 @Composable
-fun BodyContent(modifier : Modifier = Modifier){
-    Column(modifier = Modifier) {
-        Text(text = "Hi there")
-        Text(text = "Thanks for going through the Layouts codelab")
+fun ScrollingList(){
+    val listSize = 100
+    // Used to save the the scrolling position
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
 
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch{
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                // The first item index
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // The last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize)  {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyContent(modifier : Modifier = Modifier){
+    StaggeredGrid(modifier = modifier) {
+        for ( topic in  topics) {
+            Chip(modifier = Modifier.padding(8.dp), text = topic)
+        }
     }
 }
 
 @Composable
 fun SimpleList(){
-    // We save the scrolling position with this state that can also
-    // be used to programmatically scroll the list
+    // We save the scrolling position with this state
     val scrollState = rememberLazyListState()
 
     LazyColumn(state = scrollState){
@@ -90,26 +136,198 @@ fun SimpleList(){
     }
 }
 
-@Preview(
-    widthDp = 393,
-    heightDp = 786,
-    name = "Preview on my device"
-)
+/**
+ * My custom column
+ */
 @Composable
-fun LayoutsCodelabPreview(){
-    LayoutsInJetpackComposeTheme {
-        LayoutsCodelab()
+fun CustomLayout(
+    modifier: Modifier = Modifier,
+    // Custom layout attributes
+    content: @Composable () -> Unit
+){
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            //Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed the children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight){
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative( x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
     }
 }
 
-@Preview(
-    widthDp = 350,
-    heightDp = 350,
-    name = "ScrollableList"
+/**
+ * My custom layout
+ */
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then (
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check if the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
+        }
+
+    }
+        )
+
+
+/**
+ * My custom staggered grid
+ */
+@Composable
+fun StaggeredGrid (
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout (
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+
+        // Keep track of the width of each rows
+        val rowWidths = IntArray(rows) { 0 }
+
+        // Keep track of the heights of each row
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+
+            // Measure each child
+            val placeable = measurable.measure(constraints)
+
+            // Track the width and max height of each row
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        // Grid's width is the widest row
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+            ?: constraints.minWidth
+
+        // Grid's height is the sum of the tallest element in each row
+        // coerced to the height constraints
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        // Y of each row, based on the height accumulation of previous rows
+        val rowY = IntArray(rows) { 0 }
+        for(i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
 )
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Row (
+            modifier = Modifier.padding( start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box (
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview(widthDp = 393, heightDp = 786, name = "Pixel 3 preview ")
+@Composable
+fun LayoutsCodelabPreview(){
+    LayoutsInJetpackComposeTheme {
+        BodyContent()
+    }
+}
+
+//@Preview(widthDp = 393, heightDp = 786, name = "Pixel 3 preview ")
 @Composable
 fun ListPreview(){
     LayoutsInJetpackComposeTheme {
-        SimpleList()
+        ScrollingList()
+    }
+}
+
+//@Preview(widthDp = 393, heightDp = 786, name = "Pixel 3 preview ")
+@Composable
+fun TextWithPaddingToBaselinePreview(){
+    LayoutsInJetpackComposeTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+//@Preview(widthDp = 393, heightDp = 786, name = "Pixel 3 preview ")
+@Composable
+fun TextWithNormalPaddingPreview(){
+    LayoutsInJetpackComposeTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun ChipPreview(){
+    LayoutsInJetpackComposeTheme {
+        Chip(text = "Hello Bamidele")
     }
 }
